ecb8ec7d180f67dfff888233858d8da4dcee2fec
Adil Sikandar <adil.sikandar@mobilelive.ca>
2021-07-25 12:46:07 +0500

    added real domain name in the config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     WS_BASE_URL: 'https://dfefch49ewzyg.cloudfront.net/',
-    DOMAIN_NAME: 'https://master.dfli2d1chdcab.amplifyapp.com/',
+    DOMAIN_NAME: 'https://www.revelecreators.com/',
     DEMO: true
 }
 
