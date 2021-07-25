c138518a3501ad69f5270f6c04b946ae48692f01
Adil Sikandar <adil.sikandar@mobilelive.ca>
2021-07-25 15:23:33 +0500

    f

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
     WS_BASE_URL: 'https://dfefch49ewzyg.cloudfront.net/',
+    WS_BASE_URL_LOCAL: 'http://localhost:5100/api/',
     DOMAIN_NAME: 'https://www.revelecreators.com/',
     DEMO: true
 }
